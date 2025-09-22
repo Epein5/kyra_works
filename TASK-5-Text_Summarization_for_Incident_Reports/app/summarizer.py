1370566cfb8ee889d0f054bb1e8e0bb14f0a90f0
Epein5 <aayushgtm5@gmail.com>
2025-09-22 18:54:16 +0545

    minor changes

--- TASK-5-Text_Summarization_for_Incident_Reports/app/summarizer.py
+++ TASK-5-Text_Summarization_for_Incident_Reports/app/summarizer.py
@@ -1,6 +1,11 @@
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+import nltk
+
+# Download required NLTK data
+nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
 
 def summarize_text(text, sentences_count=3):
     parser = PlaintextParser.from_string(text, Tokenizer("english"))
